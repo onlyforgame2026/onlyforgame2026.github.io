8b48ec158b430aa2f7d27e4d39c38932743273ee
onlyforgame2026 <sickgo1995@gmail.com>
2026-07-21 04:04:18 +0800

    Enable comments and instant reactions

--- serverbloom/anonymous/assets/post.js
+++ serverbloom/anonymous/assets/post.js
@@ -319,7 +319,7 @@
           renderReactionButtons(buttons, state.counts, state.selected);
           window.alert("Emoji 暫時無法送出，請稍後再試。");
         } finally {
-          yncing = false;
+          syncing = false;
         }
       });
     });
